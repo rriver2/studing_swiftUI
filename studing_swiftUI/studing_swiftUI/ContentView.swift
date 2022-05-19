d379418142b17b7458799cfcd6da7761a498d4e8
이가은 <82457928+rriver2@users.noreply.github.com>
2022-05-19 21:55:04 +0900

    first commit

---
+++ studing_swiftUI/studing_swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  studing_swiftUI
+//
+//  Created by 이가은 on 2022/05/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ studing_swiftUI/studing_swiftUI/studing_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  studing_swiftUIApp.swift
+//  studing_swiftUI
+//
+//  Created by 이가은 on 2022/05/19.
+//
+
+import SwiftUI
+
+@main
+struct studing_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
